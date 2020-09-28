f21b81eb7efd0e436e59f30ea958fd5c13c04380
vinoth777kumar <vinothkumar@wavefrontech.com>
2020-09-28 13:04:38 +0530

    changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://192.168.43.55/bottesford_api/';
+	public $baseURL = 'http://192.168.43.55/bottesford_backend/';
 
 	/*
 	|--------------------------------------------------------------------------
